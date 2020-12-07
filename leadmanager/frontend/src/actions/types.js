1b6fc3c4ba3b7b5c7da5a5a677bedbb83477c102
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-12-07 18:23:28 +0900

    React / ADD auth state & private routes

--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -3,5 +3,7 @@ export const DELETE_LEADS = 'DELETE_LEADS';
 export const ADD_LEAD = 'ADD_LEAD';
 export const GET_ERRORS = 'GET_ERRORS';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
-
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
 
